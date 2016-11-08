8af7bf074db4b3ff8a5eac088a7b6b4634bae635
Peter de Souza <peterdesouza@gmail.com>
2016-11-08 14:31:34 +0000

    added exercise history class for logic to be centrally accessible

--- app/src/main/java/com/zonesciences/pyrros/helper/ExerciseHistory.java
+++ app/src/main/java/com/zonesciences/pyrros/helper/ExerciseHistory.java
@@ -1,15 +1,6 @@
-package com.zonesciences.pyrros.fragment;
+package com.zonesciences.pyrros.helper;
 
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.support.v4.app.Fragment;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -17,21 +8,18 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
-import com.zonesciences.pyrros.R;
 import com.zonesciences.pyrros.models.Exercise;
 import com.zonesciences.pyrros.models.Workout;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
+/**
+ * Created by Peter on 08/11/2016.
+ */
+public class ExerciseHistory {
 
-public class ExerciseHistoryFragment extends Fragment {
-
-    private static final String ARG_EXERCISE_KEY = "ExerciseKey";
-    private static final String ARG_USER_ID = "UserId";
-    private static final String TAG = "ExerciseHistoryFragment";
+    private static final String TAG = "ExerciseHistory.class";
 
     DatabaseReference mUserWorkoutExercisesRef;
 
@@ -41,26 +29,10 @@ public class ExerciseHistoryFragment extends Fragment {
     List<Exercise> mExercises = new ArrayList<>();
     List<String> mExerciseDates = new ArrayList<>();
 
-    public static ExerciseHistoryFragment newInstance(String exerciseKey, String userId) {
-        Bundle args = new Bundle();
-        args.putString(ARG_EXERCISE_KEY, exerciseKey);
-        args.putString(ARG_USER_ID, userId);
-        ExerciseHistoryFragment fragment = new ExerciseHistoryFragment();
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    public ExerciseHistoryFragment() {
-        // Required empty public constructor
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
-        Log.i(TAG, "onCreate()");
-        Bundle bundle = getArguments();
-        mExerciseKey = bundle.getString(ARG_EXERCISE_KEY);
-        mUserId = bundle.getString(ARG_USER_ID);
+    public ExerciseHistory(DatabaseReference userWorkoutExercisesRef, String userId, String exerciseKey){
+        mUserWorkoutExercisesRef = userWorkoutExercisesRef;
+        mExerciseKey = exerciseKey;
+        mUserId = userId;
 
         mUserWorkoutExercisesRef = FirebaseDatabase.getInstance().getReference().child("user-workout-exercises").child(mUserId);
         //Firebase deep path query returns all workouts containing mExerciseKey
@@ -72,7 +44,7 @@ public class ExerciseHistoryFragment extends Fragment {
 
                 Log.i(TAG, "Datasnapshot instances: " + dataSnapshot.getChildrenCount() + " VALUE : " + dataSnapshot.getValue());
                 for (DataSnapshot workout : dataSnapshot.getChildren()){
-                    getExerciseDate(workout.getKey());
+                    getWorkoutDates(workout.getKey());
                     Log.i(TAG, "Workout key: " + workout.getKey());
                     for (DataSnapshot exercise : workout.getChildren()){
 
@@ -96,7 +68,7 @@ public class ExerciseHistoryFragment extends Fragment {
 
     }
 
-    private void getExerciseDate(final String workoutKey) {
+    private void getWorkoutDates(final String workoutKey) {
         mUserWorkoutExercisesRef.getRoot().child("user-workouts").child(mUserId).child(workoutKey).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot workout) {
@@ -112,16 +84,11 @@ public class ExerciseHistoryFragment extends Fragment {
         });
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        Log.i(TAG, "onCreateView()");
-
-        View rootView =  inflater.inflate(R.layout.fragment_exercise_history, container, false);
-
-        TextView tv = (TextView) rootView.findViewById(R.id.textview_exercise_history);
-        tv.setText("Exercise history for: " + mExerciseKey);
-
-        return rootView;
+    public List<Exercise> getExercises() {
+        return mExercises;
     }
 
+    public List<String> getExerciseDates() {
+        return mExerciseDates;
+    }
 }
