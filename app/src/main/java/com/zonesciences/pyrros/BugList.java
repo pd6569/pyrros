b1851c673c9ce1d0d53344d61a00aaa3b19ac39b
Peter de Souza <peterdesouza@gmail.com>
2016-11-26 21:03:44 +0000

    added bug list file

---
+++ app/src/main/java/com/zonesciences/pyrros/BugList.java
@@ -0,0 +1,9 @@
+package com.zonesciences.pyrros;
+
+/**
+ * Created by Peter on 26/11/2016.
+ */
+public class BugList {
+
+    //TODO: records not updated if workout date is changed for a workout in which a record is set - date remains
+}
