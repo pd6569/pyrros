686858e877521f0af9228bf783c92dd7ce9d8583
Peter de Souza <peterdesouza@gmail.com>
2016-12-03 08:31:54 +0000

    added sort workout adapter

---
+++ app/src/main/java/com/zonesciences/pyrros/adapters/SortWorkoutAdapter.java
@@ -0,0 +1,37 @@
+package com.zonesciences.pyrros.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Peter on 03/12/2016.
+ */
+public class SortWorkoutAdapter extends RecyclerView.Adapter<SortWorkoutAdapter.SortWorkoutViewHolder> {
+
+    public class SortWorkoutViewHolder extends RecyclerView.ViewHolder {
+
+        public SortWorkoutViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+    @Override
+    public SortWorkoutViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(SortWorkoutViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+
+
+
+}
--- app/src/main/java/com/zonesciences/pyrros/fragment/CreateWorkout/SortWorkoutFragment.java
+++ app/src/main/java/com/zonesciences/pyrros/fragment/CreateWorkout/SortWorkoutFragment.java
@@ -3,16 +3,17 @@ package com.zonesciences.pyrros.fragment.CreateWorkout;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.zonesciences.pyrros.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class SortWorkoutFragment extends Fragment {
+
+    // View
+    RecyclerView mRecyclerView;
 
 
     public SortWorkoutFragment() {
@@ -23,8 +24,11 @@ public class SortWorkoutFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sort_workout, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_sort_workout, container, false);
+
+        return rootView;
     }
+
 
 }
