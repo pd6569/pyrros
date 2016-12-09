85cfbb02320c8a53a7cc6be08cbecd23d4eb4e4f
Peter de Souza <peterdesouza@gmail.com>
2016-12-09 20:10:26 +0000

    added to bugslist. updated spinner in create workout layout

--- app/src/main/java/com/zonesciences/pyrros/BugList.java
+++ app/src/main/java/com/zonesciences/pyrros/BugList.java
@@ -5,7 +5,16 @@ package com.zonesciences.pyrros;
  */
 public class BugList {
 
+    //BUGS
+
     //TODO: records not updated if workout date is changed for a workout in which a record is set - date remains
     //TODO: Opening workout from within workout (i.e. from stats) cannot navigate back to previous workout
     //TODO: Deleting workout does not delete record
+
+    // Things for improvement
+
+    // Sharing / copying workouts - how to track and credit this. What data to transfer when copying.
+    // E.g. ? copy set/reps information ?
+
+
 }
