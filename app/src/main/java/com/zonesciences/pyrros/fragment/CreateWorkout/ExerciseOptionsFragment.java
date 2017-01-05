233a04a00899af817fefca1e03edf789f511188f
Peter de Souza <peterdesouza@gmail.com>
2017-01-05 09:58:24 +0000

    changed exercise options layout and added click listener to titles

--- app/src/main/java/com/zonesciences/pyrros/fragment/CreateWorkout/ExerciseOptionsFragment.java
+++ app/src/main/java/com/zonesciences/pyrros/fragment/CreateWorkout/ExerciseOptionsFragment.java
@@ -31,7 +31,7 @@ import java.util.List;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ExerciseOptionsFragment extends Fragment {
+public class ExerciseOptionsFragment extends Fragment implements View.OnClickListener {
 
     private static final String TAG = "ExerciseOptionsFragment";
 
@@ -44,12 +44,14 @@ public class ExerciseOptionsFragment extends Fragment {
     int mNumReps;
 
     // Rep Tempo
+    TextView mRepTempoTitle;
     TextView mRepTempoText;
     NumberPicker mEccentricNumberPicker;
     NumberPicker mPauseNumberPicker;
     NumberPicker mConcentricPicker;
 
     // Rest Interval
+    TextView mRestIntervalTitle;
     TextView mRestIntervalText;
     NumberPicker mRestIntervalMinutesNumberPicker;
     NumberPicker mRestIntervalSecondsNumberPicker;
@@ -115,35 +117,29 @@ public class ExerciseOptionsFragment extends Fragment {
         mRecyclerView.setAdapter(mAdapter);
 
         // Rep Tempo
+        mRepTempoTitle = (TextView) view.findViewById(R.id.exercise_options_rep_tempo_title_textview);
+        mRepTempoTitle.setOnClickListener(this);
         mRepTempoText = (TextView) view.findViewById(R.id.exercise_options_rep_tempo_textview);
         if (mExercise.getRepTempo() != null) {
             mRepTempoText.setText(mExercise.getRepTempo());
         } else {
             mRepTempoText.setText("Not set");
         }
-        mRepTempoText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showRepTempoDialog();
-            }
-        });
+        mRepTempoText.setOnClickListener(this);
 
         // Rest Interval
+        mRestIntervalTitle = (TextView) view.findViewById(R.id.exercise_options_rest_interval_title_textview);
+        mRestIntervalTitle.setOnClickListener(this);
+
         mRestIntervalText = (TextView) view.findViewById(R.id.exercise_options_rest_interval_textview);
         if (mExercise.getRestInterval() == 0){
             mRestIntervalText.setText("Not set");
         } else {
             mRestIntervalText.setText(Utils.timeToDisplay(mExercise.getRestInterval() * 1000).get(Utils.MINUTES) + ":" + Utils.timeToDisplay(mExercise.getRestInterval() * 1000).get(Utils.SECONDS));
         }
-        mRestIntervalText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showRestIntervalDialog();
-            }
-        });
+        mRestIntervalText.setOnClickListener(this);
 
         // Add sets
-
         mNumSetsText = (TextView) view.findViewById(R.id.exercise_options_num_sets_textview);
         updateSetsInfo();
 
@@ -337,124 +333,15 @@ public class ExerciseOptionsFragment extends Fragment {
         alertDialog.show();
     }
 
-    /*@Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.action_workout_changes_complete){
-            Log.i(TAG, "Workout changes complete");
-            return true;
+
+    // Click listener methods
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.exercise_options_rep_tempo_textview || view.getId() == R.id.exercise_options_rep_tempo_title_textview) {
+            showRepTempoDialog();
         }
-        return super.onOptionsItemSelected(item);
-    }*/
+        if (view.getId() == R.id.exercise_options_rest_interval_textview || view.getId() == R.id.exercise_options_rest_interval_title_textview) {
+            showRestIntervalDialog();
+        }
+    }
 }
-
-
-/*
-                    // Sets
-                    mSets = mWorkoutExercises.get(getAdapterPosition()).getPrescribedReps();
-
-                    if (mSets != null) {
-                        Log.i(TAG, "mSets" + mSets.size());
-                    }
-
-                    Log.i(TAG, "Open dialog options");
-                    LayoutInflater inflater = LayoutInflater.from(mActivity);
-                    View dialogView = inflater.inflate(R.layout.fragment_exercise_options, null);
-                    AlertDialog.Builder builder = new AlertDialog.Builder(mActivity);
-                    builder.setView(dialogView);
-
-                    // Find views
-                    TextView dialogTitle = (TextView) dialogView.findViewById(R.id.dialog_exercise_options_exercise_title_textview);
-                    dialogTitle.setText(mWorkoutExercises.get(getAdapterPosition()).getName());
-                    ImageView closeDialog = (ImageView) dialogView.findViewById(R.id.dialog_exercise_options_close_imageview);
-
-                    //RecyclerView
-                    RecyclerView recyclerView = (RecyclerView)  dialogView.findViewById(R.id.dialog_exercise_options_add_sets_recycler);
-                    recyclerView.setLayoutManager(new LinearLayoutManager(mActivity));
-                    recyclerView.setHasFixedSize(true);
-                    DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(mActivity, DividerItemDecoration.VERTICAL);
-                    recyclerView.addItemDecoration(dividerItemDecoration);
-                    if (mSets == null){
-                        mSets = new ArrayList<Integer>();
-                    }
-                    final AddSetExerciseOptionsAdapter adapter = new AddSetExerciseOptionsAdapter(mActivity, mSets);
-                    recyclerView.setAdapter(adapter);
-
-
-                    final EditText numRepsField = (EditText) dialogView.findViewById(R.id.exercise_options_num_reps_edit_text);
-                    Button decreaseReps = (Button) dialogView.findViewById(R.id.exercise_options_decrease_reps_button);
-                    decreaseReps.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            if (!numRepsField.getText().toString().isEmpty()) {
-                                mNumReps = Integer.parseInt(numRepsField.getText().toString());
-                            } else {
-                                mNumReps = 0;
-                            }
-                            if (mNumReps > 0){
-                                mNumReps--;
-                            }
-                            numRepsField.setText("" + mNumReps);
-                        }
-                    });
-                    Button increaseReps = (Button) dialogView.findViewById(R.id.exercise_options_increase_reps_button);
-                    increaseReps.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            if (!numRepsField.getText().toString().isEmpty()) {
-                                mNumReps = Integer.parseInt(numRepsField.getText().toString());
-                            } else {
-                                mNumReps = 0;
-                            }
-                            mNumReps++;
-                            numRepsField.setText("" + mNumReps);
-                        }
-                    });
-
-                    Button addSet = (Button) dialogView.findViewById(R.id.exercise_options_add_set);
-                    addSet.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            if (mSets == null){
-                                mSets = new ArrayList<Integer>();
-                            }
-                            mSets.add(Integer.parseInt(numRepsField.getText().toString()));
-                            adapter.notifyDataSetChanged();
-                        }
-                    });
-
-                    builder
-                            .setCancelable(false)
-                            .setPositiveButton("OK", new DialogInterface.OnClickListener(){
-                                public void onClick(DialogInterface dialogBox, int id){
-                                    Log.i(TAG, "OK");
-                                    if (!adapter.getSets().isEmpty()){
-                                        mWorkoutExercises.get(getAdapterPosition()).setPrescribedReps(adapter.getSets());
-
-                                    }
-                                }
-                            })
-                            .setOnDismissListener(new DialogInterface.OnDismissListener() {
-                                @Override
-                                public void onDismiss(DialogInterface dialog) {
-                                    Log.i(TAG, "onDismiss");
-                                    if (!adapter.getSets().isEmpty()){
-                                        mWorkoutExercises.get(getAdapterPosition()).setPrescribedReps(adapter.getSets());
-                                    }
-                                    mSets = null;
-                                }
-                            })
-                            .setNegativeButton("Cancel", new DialogInterface.OnClickListener(){
-                                public void onClick(DialogInterface dialogBox, int id){
-                                    dialogBox.cancel();
-                                    mSets = null;
-                                }
-                            });
-                    final AlertDialog alertDialog = builder.create();
-                    alertDialog.show();
-
-                    closeDialog.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            alertDialog.dismiss();
-                        }
-                    });*/
